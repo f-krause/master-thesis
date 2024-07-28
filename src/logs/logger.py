cde95aed6bae5fcad2b9aff6c01caca1a1e7abf9
Felix Krause <felix_krause1999@yahoo.de>
2024-07-28 13:10:13 +0200

    fixing duplicate logging code

--- src/logs/logger.py
+++ src/logs/logger.py
@@ -8,21 +8,22 @@ def setup_logger():
     logger = logging.getLogger()
     logger.setLevel(logging.DEBUG)
 
-    # File handler
-    fh = logging.FileHandler(log_file)
-    fh.setLevel(logging.DEBUG)
+    if not logger.handlers:
+        # File handler
+        fh = logging.FileHandler(log_file)
+        fh.setLevel(logging.DEBUG)
 
-    # Console handler
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.INFO)
+        # Console handler
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.INFO)
 
-    # Formatter
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    fh.setFormatter(formatter)
-    ch.setFormatter(formatter)
+        # Formatter
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        fh.setFormatter(formatter)
+        ch.setFormatter(formatter)
 
-    # Add handlers
-    logger.addHandler(fh)
-    logger.addHandler(ch)
+        # Add handlers
+        logger.addHandler(fh)
+        logger.addHandler(ch)
 
     return logger
