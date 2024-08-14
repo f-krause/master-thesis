9761e65835f3fb853d4b2aa2c41ae6bfe03c8615
krausef99 <krausef99@univie.ac.at>
2024-08-14 12:41:07 +0200

    adding sparse matrix storage and more - in dev state

--- src/data_handling/pred_secondary_struc.py
+++ src/data_handling/pred_secondary_struc.py
@@ -8,23 +8,26 @@ load_dotenv()
 import os
 import pickle
 import logging
+from tqdm import tqdm
+from scipy.sparse import csr_matrix
 from arnie.bpps import bpps
 from arnie.mea.mea import MEA
 
 FOLD_PACKAGES = ['vienna_2']  # see doc for more packages: https://github.com/DasLab/arnie/blob/master/docs/setup_doc.md
-SEC_STRUC_PATH = '../../data/sec_struc/'
+SEC_STRUC_PATH = '/mnt/data/krausef99dm_thesis/data/sec_struc/'
 
+OVERWRITE_FILES = True  # FIXME for dev only
 PRED_BPP = True
 PRED_STRUCTURE = False
 PRED_LOOP_TYPE = False
 
+logging.FileHandler("pred_sec_struc.log")
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger()
 
 
 # Compute BPPs
-# TODO create separate file to create BPPs or loop type/structure once and save them
 def _pred_bpps(idx, folding_package, seq):
     try:
         # data[transcript]['fasta']
@@ -38,9 +41,10 @@ def _pred_structure(bpp_matrix):
     return MEA(bpp_matrix).structure
 
 
-def _pred_loop_type(seq, structure, debug=True):
-    # TODO install perl and Graph.pm to run bpRNA
-    os.system("cd folding_algorithms/bpRNA")
+def _pred_loop_type(seq, structure, debug=False):
+    # return None
+    # os.system("cd folding_algorithms/bpRNA")
+    os.chdir("folding_algorithms/bpRNA")
     os.system(f'echo {seq} > a.dbn')
     os.system('echo \"{}\" >> a.dbn'.format(structure))
     os.system('perl bpRNA.pl a.dbn')
@@ -49,6 +53,7 @@ def _pred_loop_type(seq, structure, debug=True):
         print(seq)
         print(structure)
         print(loop_type[5])
+    os.chdir("/export/home/krausef99dm/master-thesis/src/data_handling")
     return loop_type
 
 
@@ -69,25 +74,35 @@ def _store_preds(idx, fold_package, preds):
 def main():
     logging.info("Predicting secondary structure and loop type.")
 
-    with open('../../data/ptr_data.pkl', 'rb') as f:
+    with open('/mnt/data/krausef99dm_thesis/data/ptr_data.pkl', 'rb') as f:
+        # TODO make sure to drop super long sequences from data!
         data = pickle.load(f)
 
     ids = data.keys()
 
-    # for idx in ids:
+    # for idx in tqdm(ids):
     for idx in ["ENST00000304312"]:  # FIXME dev
         preds = {fold_package: {'bpp': None, 'structure': None, 'loop_type': None} for fold_package in FOLD_PACKAGES}
         seq = data[idx]['fasta']
 
         for fold_package in FOLD_PACKAGES:
-            if _check_file_exists(idx, fold_package):
+            if _check_file_exists(idx, fold_package) and not OVERWRITE_FILES:
                 continue
+
             logging.info(f"Computing predictions for: {idx}-{fold_package}")
-            preds[fold_package]['bpp'] = _pred_bpps(idx, fold_package, seq)
 
-            preds[fold_package]['structure'] = _pred_structure(preds[fold_package]['bpp'])
+            bpp_temp = _pred_bpps(idx, fold_package, seq)
 
-            # preds[fold_package]['loop_type'] = _pred_loop_type(seq, preds[fold_package]['structure'])
+            preds[fold_package]['structure'] = _pred_structure(bpp_temp)
+
+            preds[fold_package]['loop_type'] = _pred_loop_type(seq, preds[fold_package]['structure'])
+
+            # store bpp as sparse matrix
+
+            bpp_temp[bpp_temp < 0.001] = 0  # set all pairing probabilities below 0.001 to 0
+            bpp_temp_sparse = csr_matrix(bpp_temp)
+
+            preds[fold_package]['bpp'] = bpp_temp_sparse
 
             _store_preds(idx, fold_package, preds)
         break
