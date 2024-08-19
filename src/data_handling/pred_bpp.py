5649b8a3b9fa3fd535d28a7a4d3bbd57ea650eba
krausef99 <krausef99@univie.ac.at>
2024-08-19 16:47:35 +0200

    updating bpp prediction code

--- src/data_handling/pred_bpp.py
+++ src/data_handling/pred_bpp.py
@@ -7,33 +7,45 @@ load_dotenv()
 
 import os
 import pickle
+import json
 import logging
+import time
 from tqdm import tqdm
 from scipy.sparse import csr_matrix
 from arnie.bpps import bpps
 from arnie.mea.mea import MEA
 
-FOLD_PACKAGES = ['vienna_2']  # see doc for more packages: https://github.com/DasLab/arnie/blob/master/docs/setup_doc.md
-SEC_STRUC_PATH = '/mnt/data/krausef99dm_thesis/data/sec_struc/'
+FOLD_PACKAGE = 'vienna_2'  # see doc for more packages: https://github.com/DasLab/arnie/blob/master/docs/setup_doc.md
+DATA_PATH = '/mnt/data/krausef99dm_thesis/data'
 
 OVERWRITE_FILES = True  # FIXME for dev only
-PRED_BPP = True
-PRED_STRUCTURE = False
-PRED_LOOP_TYPE = False
+MAX_SEQ_LENGTH = 400  # 3241 seq with len below 2000
+MAX_PRED_NR = 2
 
-logging.FileHandler("pred_sec_struc.log")
 logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    handlers=[
+                        # add time stamp to log file
+                        logging.FileHandler(f"logs/pred_bpp_{time.strftime('%Y%m%d-%H%M')}.log"),
+                        logging.StreamHandler()
+                    ])
 logger = logging.getLogger()
 
 
+def _load_data():
+    with open(os.path.join(DATA_PATH, 'ptr_data.pkl'), 'rb') as f:
+        # TODO make sure to drop super long sequences from data!
+        data = pickle.load(f)
+    return data
+
+
 # Compute BPPs
-def _pred_bpps(idx, folding_package, seq):
+def _pred_bpps(idx, seq):
     try:
         # data[transcript]['fasta']
-        return bpps(seq, package=folding_package)
+        return bpps(seq, package=FOLD_PACKAGE)
     except Exception as e:
-        logger.error(f"{idx}: Could not compute BPPs with package {folding_package}.")
+        logger.error(f"{idx}: Could not compute BPPs with package {FOLD_PACKAGE}.")
         logger.error(e)
 
 
@@ -42,7 +54,6 @@ def _pred_structure(bpp_matrix):
 
 
 def _pred_loop_type(seq, structure, debug=False):
-    # return None
     # os.system("cd folding_algorithms/bpRNA")
     os.chdir("folding_algorithms/bpRNA")
     os.system(f'echo {seq} > a.dbn')
@@ -57,16 +68,16 @@ def _pred_loop_type(seq, structure, debug=False):
     return loop_type
 
 
-def _check_file_exists(idx, fold_package):
-    path = os.path.join(SEC_STRUC_PATH, f'{idx}-{fold_package}.pkl')
+def _check_file_exists(idx):
+    path = os.path.join(DATA_PATH, "bpp", f'{idx}-{FOLD_PACKAGE}.json')
     if os.path.exists(path):
         logging.warning(f"Skipping {idx}. File {path} already exists.")
         return True
     return False
 
 
-def _store_preds(idx, fold_package, preds):
-    path = os.path.join(SEC_STRUC_PATH, f'{idx}-{fold_package}.pkl')
+def _store_preds(idx, preds):
+    path = os.path.join(DATA_PATH, "bpp", f'{idx}-{FOLD_PACKAGE}.json')
     with open(path, 'wb') as f:
         pickle.dump(preds, f)
 
@@ -74,38 +85,50 @@ def _store_preds(idx, fold_package, preds):
 def main():
     logging.info("Predicting secondary structure and loop type.")
 
-    with open('/mnt/data/krausef99dm_thesis/data/ptr_data.pkl', 'rb') as f:
-        # TODO make sure to drop super long sequences from data!
-        data = pickle.load(f)
-
+    data = _load_data()
     ids = data.keys()
 
-    # for idx in tqdm(ids):
-    for idx in ["ENST00000304312"]:  # FIXME dev
-        preds = {fold_package: {'bpp': None, 'structure': None, 'loop_type': None} for fold_package in FOLD_PACKAGES}
+    counter = 0
+    start_time = time.time()
+
+    # for idx in ["ENST00000304312"]:  # FIXME dev
+    for idx in tqdm(ids):
+        preds = {}
+        if counter >= MAX_PRED_NR:
+            break
+        if not OVERWRITE_FILES and _check_file_exists(idx):
+            continue
+
         seq = data[idx]['fasta']
 
-        for fold_package in FOLD_PACKAGES:
-            if _check_file_exists(idx, fold_package) and not OVERWRITE_FILES:
-                continue
+        if len(seq) > MAX_SEQ_LENGTH:
+            # logging.warning(f"Skipping {idx}. Sequence longer than {MAX_SEQ_LENGTH}.")  # FIXME for dev only
+            continue
 
-            logging.info(f"Computing predictions for: {idx}-{fold_package}")
+        logging.info(f"Computing predictions for: {idx}-{FOLD_PACKAGE}")
+        logging.info(f"   Sequence length: {len(seq)}")
 
-            bpp_temp = _pred_bpps(idx, fold_package, seq)
+        start_time_seq = time.time()
+        bpp_temp = _pred_bpps(idx, seq)
 
-            preds[fold_package]['structure'] = _pred_structure(bpp_temp)
+        preds['structure'] = _pred_structure(bpp_temp)
 
-            preds[fold_package]['loop_type'] = _pred_loop_type(seq, preds[fold_package]['structure'])
+        preds['loop_type'] = _pred_loop_type(seq, preds['structure'])
 
-            # store bpp as sparse matrix
+        # store bpp as sparse matrix
+        bpp_temp[bpp_temp < 0.001] = 0  # set all pairing probabilities below 0.001 to 0
+        bpp_temp_sparse = csr_matrix(bpp_temp)
 
-            bpp_temp[bpp_temp < 0.001] = 0  # set all pairing probabilities below 0.001 to 0
-            bpp_temp_sparse = csr_matrix(bpp_temp)
+        preds['bpp'] = bpp_temp_sparse
 
-            preds[fold_package]['bpp'] = bpp_temp_sparse
+        logging.info(f"   Pred time: {(time.time() - start_time_seq)} seconds.")
 
-            _store_preds(idx, fold_package, preds)
-        break
+        _store_preds(idx, preds)
+        counter += 1
+
+    logging.info(f"Predicted {counter} secondary structures in {(time.time() - start_time)/60} minutes.")
+    logging.info(f"Average time per prediction: {(time.time() - start_time) / counter} seconds.")
+    logging.info("Done.")
 
 
 if __name__ == "__main__":
