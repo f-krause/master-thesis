e38f2792cbac59241170ece8419f0fbc51fdd23b
krausef99 <krausef99@univie.ac.at>
2024-09-06 14:56:25 +0200

    add tissues to utils

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,13 @@ from log.logger import setup_logger
 from box import Box
 
 
+TISSUES = ['Adrenal', 'Appendices', 'Brain', 'Colon', 'Duodenum', 'Uterus',
+       'Esophagus', 'Fallopiantube', 'Fat', 'Gallbladder', 'Heart', 'Kidney',
+       'Liver', 'Lung', 'Lymphnode', 'Ovary', 'Pancreas', 'Placenta',
+       'Prostate', 'Rectum', 'Salivarygland', 'Smallintestine', 'Smoothmuscle',
+       'Spleen', 'Stomach', 'Testis', 'Thyroid', 'Tonsil', 'Urinarybladder']
+
+
 def mkdir(path: str):
     if not os.path.isdir(path):
         os.makedirs(path, exist_ok=True)
