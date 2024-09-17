0f87beed4ed242998194a6dcf691502fbe672ed4
krausef99 <krausef99@univie.ac.at>
2024-09-17 21:54:30 +0200

    update model template

--- src/models/model_template.py
+++ src/models/model_template.py
@@ -1,10 +1,12 @@
 import torch.nn as nn
 
 
-class ModelBaseline(nn.Module):
+class ModelTemplate(nn.Module):
     def __init__(self):
-        super(ModelBaseline, self).__init__()
+        super(ModelTemplate, self).__init__()
         self.layer = nn.Linear(10, 2)  # Example layer
+
+        raise NotImplementedError("Please implement the model")
 
     def forward(self, x):
         return self.layer(x)
