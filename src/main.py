653ef8a6e79bbfb2afdeff9f12d51fe315b9642f
krausef99 <krausef99@univie.ac.at>
2024-09-20 15:01:42 +0200

    add args to main

--- src/main.py
+++ src/main.py
@@ -1,15 +1,47 @@
 import os
+import yaml
+import argparse
+from box import Box
+
 from training import train, tuning
 from log.logger import setup_logger
 from utils import set_log_file, set_project_path
-import yaml
-from box import Box
 
-CONFIG_PATH = "config/config_template.yml"
+CONFIG_PATH_DEFAULT = "config/config_template.yml"
+
+parser = argparse.ArgumentParser(prog='main.py', description='Trains DL models on mRNA data to predict PTR ratios.')
+
+parser.add_argument('-c', '--custom_path', help="Path to config file", type=str, default=None)
+parser.add_argument('-b', "--baseline", help="Use baseline config", action="store_true")
+parser.add_argument('-l', "--lstm", help="Use lstm config", action="store_true")
+parser.add_argument('-x', "--xlstm", help="Use xlstm config", action="store_true")
+parser.add_argument('-t', "--transformer", help="Use transformer config", action="store_true")
+parser.add_argument('-m', "--mamba", help="Use mamba config", action="store_true")
+
+args = parser.parse_args()
+
+if args.custom_path:
+    config_path = args.custom_path
+elif args.baseline:
+    config_path = "config/config_baseline.yml"
+elif args.lstm:
+    raise NotImplementedError("LSTM config not implemented yet")
+    # config_path = "config/config_lstm.yml"
+elif args.xlstm:
+    raise NotImplementedError("XLSTM config not implemented yet")
+    # config_path = "config/config_xlstm.yml"
+elif args.transformer:
+    raise NotImplementedError("Transformer config not implemented yet")
+    # config_path = "config/config_transformer.yml"
+elif args.mamba:
+    raise NotImplementedError("Mamba config not implemented yet")
+    # config_path = "config/config_mamba.yml"
+else:
+    config_path = CONFIG_PATH_DEFAULT
 
 
 if __name__ == "__main__":
-    with open(CONFIG_PATH, 'r') as file:
+    with open(config_path, 'r') as file:
         config = Box(yaml.safe_load(file))
 
     set_project_path(config)
@@ -21,7 +53,7 @@ if __name__ == "__main__":
 
     try:
         logger.info("TRAINING TEST")
-        training.train(config)
+        train.train(config)
     except Exception as e:
         logger.error(f"Error during training: {e}")
         raise e
