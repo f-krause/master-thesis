ecd732cbd3120e4b863cc962b2aa1836876755c6
krausef99 <krausef99@univie.ac.at>
2024-09-28 16:29:37 +0200

    optimize - only load models that are needed

--- src/models/get_model.py
+++ src/models/get_model.py
@@ -1,39 +1,39 @@
 import torch
 from box import Box
 
-from models.baseline.baseline import ModelBaseline
-from models.dummy.dummy import ModelDummy
-from models.rnn.rnn import ModelRNN
-from models.xlstm.xlstm import ModelXLSTM
-from models.mamba.mamba import ModelMamba
-from models.jamba.jamba import ModelJamba
-from models.transformer.transformer import ModelTransformer
-
 
 def get_model(config: Box, device: torch.device, logger=None):
     if config.model == "dummy":
         if logger: logger.warning("Using dummy model")
+        from models.dummy.dummy import ModelDummy
         return ModelDummy(device).to(device)
     elif config.model == "baseline":
         if logger: logger.info("Using baseline model")
+        from models.baseline.baseline import ModelBaseline
         return ModelBaseline(config, device).to(device)
     elif config.model == "gru":
         if logger: logger.info("Using GRU model")
+        from models.rnn.rnn import ModelRNN
         return ModelRNN(config, device, model="gru").to(device)
     elif config.model == "lstm":
         if logger: logger.info("Using LSTM model")
+        from models.rnn.rnn import ModelRNN
         return ModelRNN(config, device, model="lstm").to(device)
     elif config.model == "xlstm":
         if logger: logger.info("Using xLSTM model")
+        from models.xlstm.xlstm import ModelXLSTM
         return ModelXLSTM(config, device).to(device)
     elif config.model == "mamba":
         if logger: logger.info("Using Mamba model")
+        from models.mamba.mamba import ModelMamba
         return ModelMamba(config, device).to(device)
     elif config.model == "jamba":
         if logger: logger.info("Using Jamba model")
+        from models.jamba.jamba import ModelJamba
         return ModelJamba(config, device).to(device)
     elif config.model == "transformer":
         if logger: logger.info("Using Transformer model")
+        from models.transformer.transformer import ModelTransformer
         return ModelTransformer(config, device).to(device)
     elif config.model == "best":
         if logger: logger.info("Using best model")
