57b89dfc22dce9442ac6d06795ca33f2241b57d9
krausef99 <krausef99@univie.ac.at>
2024-10-25 11:00:09 +0200

    adding ranger as optimizer

--- src/training/optimizer.py
+++ src/training/optimizer.py
@@ -1,4 +1,5 @@
 import torch.optim as optim
+from ranger import Ranger
 from omegaconf import DictConfig
 
 
@@ -7,6 +8,8 @@ def get_optimizer(model, optimizer_config: DictConfig):
 
     if optimizer_name == 'adam':
         return optim.Adam(model.parameters(), lr=optimizer_config.lr)
+    elif optimizer_name == 'ranger':
+        return Ranger(model.parameters(), lr=optimizer_config.lr, weight_decay=optimizer_config.weight_decay)
     elif optimizer_name == 'sgd':
         return optim.SGD(model.parameters(), lr=optimizer_config.lr, momentum=optimizer_config.momentum)
     else:
