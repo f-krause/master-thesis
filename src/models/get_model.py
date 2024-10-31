f4101314ae4d8ca3b9db355a9e2711079b95b003
krausef99 <krausef99@univie.ac.at>
2024-10-31 18:17:52 +0100

    allow to select frequency models from yml config

--- src/models/get_model.py
+++ src/models/get_model.py
@@ -3,14 +3,20 @@ from omegaconf import DictConfig
 
 
 def get_model(config: DictConfig, device: torch.device, logger=None):
+    if config.frequency_features and config.model not in ["baseline", "transformer"]:
+        raise ValueError(f"Frequency features only supported for baseline and transformer models, not {config.model}")
+
     if config.model == "dummy":
         if logger: logger.warning("Using dummy model")
         from models.dummy.dummy import ModelDummy
         return ModelDummy(device).to(device)
     elif config.model == "baseline":
-        if logger: logger.info("Using baseline model")
-        # from models.baseline.baseline_freq import ModelBaseline  # super small model + codon frequencies
-        from models.baseline.baseline import ModelBaseline
+        if config.frequency_features:
+            if logger: logger.info("Using baseline model (frequencies)")
+            from models.baseline.baseline_freq import ModelBaseline
+        else:
+            if logger: logger.info("Using baseline model")
+            from models.baseline.baseline import ModelBaseline
         return ModelBaseline(config, device).to(device)
     elif config.model == "cnn":
         from models.cnn.cnn import ModelCNN
@@ -36,9 +42,12 @@ def get_model(config: DictConfig, device: torch.device, logger=None):
         from models.jamba.jamba import ModelJamba
         return ModelJamba(config, device).to(device)
     elif config.model == "transformer":
-        if logger: logger.info("Using Transformer model")
-        # from models.transformer.transformer_freq import ModelTransformer  # codon frequencies
-        from models.transformer.transformer import ModelTransformer
+        if config.frequency_features:
+            if logger: logger.info("Using Transformer model (frequencies)")
+            from models.transformer.transformer_freq import ModelTransformer  # codon frequencies
+        else:
+            if logger: logger.info("Using Transformer model")
+            from models.transformer.transformer import ModelTransformer
         return ModelTransformer(config, device).to(device)
     elif config.model == "best":
         if logger: logger.info("Using best model")
