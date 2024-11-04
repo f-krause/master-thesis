dfe0f0c6b8e2d0f6c8f0dcbb61f5d64011341df9
krausef99 <krausef99@univie.ac.at>
2024-11-04 20:48:49 +0100

    update model inputs

--- src/models/mamba/mamba.py
+++ src/models/mamba/mamba.py
@@ -58,6 +58,10 @@ class ModelMamba(nn.Module):
         # Concat embeddings (batch_size, seq_len, embedding_dim)
         combined_embedding = torch.cat((seq_embedding, tissue_embedding_expanded), dim=2)
 
+        # Remove tissue embeddings after the sequence ends
+        attention_mask = (rna_data_pad != 0).unsqueeze(-1).to(self.device)
+        combined_embedding = combined_embedding * attention_mask
+
         # Apply Mamba model
         if isinstance(self.mamba, Mamba):
             out = self.mamba(combined_embedding)
