879dde0b7295ae885f7c7f9973e9c58f7e3b0a4c
krausef99 <krausef99@univie.ac.at>
2024-11-20 21:48:46 +0100

    refactor

--- src/models/baseline/baseline_freq.py
+++ src/models/baseline/baseline_freq.py
@@ -38,11 +38,11 @@ class ModelBaseline(nn.Module):
     def forward(self, inputs):
         rna_data, tissue_id = inputs[0], inputs[1]  # (batch_size, padded_seq_length), upper bounded by max length
 
-        rna_data_pad = self._compute_frequencies(rna_data)
+        rna_data_freq = self._compute_frequencies(rna_data)
 
         tissue_embedding = self.tissue_encoder(tissue_id)
 
-        x = torch.cat((tissue_embedding, rna_data_pad), dim=1)
+        x = torch.cat((tissue_embedding, rna_data_freq), dim=1)
 
         y_pred = self.fc(x)
 
