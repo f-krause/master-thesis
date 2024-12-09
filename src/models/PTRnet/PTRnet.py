7d86bec4ec665b2a4b05d7b815fc2ce894e5088c
krausef99 <krausef99@univie.ac.at>
2024-12-09 21:29:28 +0100

    first working mamba implementation for nucleotide based data

--- src/models/PTRnet/PTRnet.py
+++ src/models/PTRnet/PTRnet.py
@@ -10,6 +10,7 @@ from knowledge_db import TISSUES, CODON_MAP_DNA
 
 from models.predictor import Predictor
 from mamba_ssm import Mamba, Mamba2
+from data_handling.train_data_seq import TOKENS
 
 
 class PTRnet(nn.Module):
@@ -23,11 +24,10 @@ class PTRnet(nn.Module):
         self.max_seq_length = config.max_seq_length
         self.dim_embedding_tissue = config.dim_embedding_tissue
         self.dim_embedding_token = config.dim_embedding_token
-        # TODO add other embeddings
 
         # Embedding layers
         self.tissue_encoder = nn.Embedding(len(TISSUES), self.dim_embedding_tissue, max_norm=config.embedding_max_norm)
-        self.seq_encoder = nn.Embedding(len(CODON_MAP_DNA) + 1, self.dim_embedding_token, padding_idx=0,
+        self.seq_encoder = nn.Embedding(len(TOKENS) + 1, self.dim_embedding_token, padding_idx=0,
                                         max_norm=config.embedding_max_norm)
 
         # Mamba model (roughly 3 * expand * d_model^2 parameters)
@@ -45,46 +45,54 @@ class PTRnet(nn.Module):
 
         self.predictor = Predictor(config, self.dim_embedding_token).to(self.device)
 
-    def forward(self, inputs: Tensor) -> Tensor:
+    def forward(self, inputs: Tensor, mask: Tensor = None) -> Tensor:
+        # rna_data.append(torch.tensor([sequence_ohe, coding_area_ohe, sec_struc_ohe, loop_type_ohe]))  # 4 x n
         rna_data_pad, tissue_id, seq_lengths = inputs[0], inputs[1], inputs[2]
 
         tissue_embedding = self.tissue_encoder(tissue_id)  # (batch_size, dim_embedding_token)
         seq_embedding = self.seq_encoder(rna_data_pad)  # (batch_size, seq_len, dim_embedding_token)
 
+        # sum up the embeddings of the 4 features (nucleotide, coding_area, sec_struc, loop_type)
+        seq_embedding = seq_embedding.sum(dim=2)
+
+
         tissue_embedding_expanded = tissue_embedding.unsqueeze(1).expand(-1, seq_embedding.size(1), -1)
 
         x = seq_embedding + tissue_embedding_expanded  # (batch_size, padded_seq_length, dim_embedding_token)
 
-        mask = (rna_data_pad != 0).unsqueeze(-1).to(self.device)
-        x *= mask
+        padding_mask = (seq_embedding != 0).to(self.device)
+        x *= padding_mask
 
         # Apply Mamba model
         for layer in self.mamba_layers:
             x = layer(x)
 
-        # Extract outputs corresponding to the last valid time step
-        idx = ((seq_lengths - 1).unsqueeze(1).unsqueeze(2).expand(-1, 1, x.size(2)))  # (batch_size, 1, embedding_dim)
-        x_last = x.gather(1, idx).squeeze(1)  # (batch_size, embedding_dim)
+        if mask:
+            # given mask, assuming pre-training
+            pass
+        else:
+            # Extract outputs corresponding to the last valid time step
+            idx = ((seq_lengths - 1).unsqueeze(1).unsqueeze(2).expand(-1, 1, x.size(2)))  # (batch_size, 1, embedding_dim)
+            x_last = x.gather(1, idx).squeeze(1)  # (batch_size, embedding_dim)
 
-        y_pred = self.predictor(x_last)
+            y_pred = self.predictor(x_last)
 
-        return y_pred
+            return y_pred
 
 
 if __name__ == "__main__":
     # Test forward pass
     device = torch.device("cuda:0")
     config_dev = OmegaConf.load("config/PTRnet.yml")
-    config_dev = OmegaConf.merge(config_dev, {"binary_class": True, "max_seq_length": 8100,
-                                              "embedding_max_norm": 2, "gpu_id": 0})
+    config_dev = OmegaConf.merge(config_dev, {"binary_class": True, "max_seq_length": 1000,
+                                                      "embedding_max_norm": 2, "gpu_id": 0})
 
     sample_batch = [
-        # rna_data_padded (batch_size x max_seq_length)
-        torch.nn.utils.rnn.pad_sequence(torch.randint(1, 64, (config_dev.batch_size, config_dev.max_seq_length)),
-                                        batch_first=True),
+        torch.nn.utils.rnn.pad_sequence(torch.randint(1, len(TOKENS) + 1, (config_dev.batch_size, config_dev.max_seq_length, 4)),
+                                        batch_first=True),  # rna_data_padded (batch_size x max_seq_length)
         torch.randint(29, (config_dev.batch_size,)),  # tissue_ids (batch_size x 1)
-        torch.tensor([config_dev.max_seq_length] * config_dev.batch_size, dtype=torch.int64)
-        # seq_lengths (batch_size x 1)
+        torch.tensor([config_dev.max_seq_length] * config_dev.batch_size,
+                     dtype=torch.int64)  # seq_lengths (batch_size x 1)
     ]
 
     sample_batch = [tensor.to(torch.device(device)) for tensor in sample_batch]
