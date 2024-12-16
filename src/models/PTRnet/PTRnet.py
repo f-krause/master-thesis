fdf48724807ee242e9c2294feea9159e2102911f
krausef99 <krausef99@univie.ac.at>
2024-12-16 21:51:36 +0100

    [doc] code for ptrnet tuning

--- src/models/PTRnet/PTRnet.py
+++ src/models/PTRnet/PTRnet.py
@@ -14,9 +14,9 @@ from mamba_ssm import Mamba, Mamba2
 from data_handling.train_data_seq import TOKENS
 
 
-class PTRnet(nn.Module):
+class PTRnetPure(nn.Module):
     def __init__(self, config: DictConfig, device: torch.device):
-        super(PTRnet, self).__init__()
+        super(PTRnetPure, self).__init__()
 
         if config.gpu_id != 0 and config.model.lower() == "mamba2":
             raise Exception("Currently Mamba2 only supports the default GPU (cuda:0)!")
@@ -24,7 +24,9 @@ class PTRnet(nn.Module):
         self.device = device
         self.max_seq_length = config.max_seq_length
         self.frequency_features = config.frequency_features
+        self.seq_only = config.seq_only
         self.pretrain = config.pretrain
+        self.nr_layers = config.num_layers
         self.dim_embedding_tissue = config.dim_embedding_tissue
         self.dim_embedding_token = config.dim_embedding_token
 
@@ -33,13 +35,17 @@ class PTRnet(nn.Module):
         else:
             self.output_dim = self.dim_embedding_token
 
-        # Embedding layers of input features
-        nr_tokens = len(TOKENS) + 2  # + 1 for padding, one for pretraining MASK token
+        nr_tokens = len(TOKENS) + 2
         self.tissue_encoder = nn.Embedding(len(TISSUES), self.dim_embedding_tissue, max_norm=config.embedding_max_norm)
         self.seq_encoder = nn.Embedding(nr_tokens, self.dim_embedding_token, padding_idx=0,
                                         max_norm=config.embedding_max_norm)
 
-        # Mamba model (roughly 3 * expand * d_model^2 parameters)
+        # Layer norms
+        if self.nr_layers > 1:
+            self.mamba_norms = nn.ModuleList([nn.LayerNorm(self.dim_embedding_token) for _ in range(self.nr_layers)])
+            self.layer_norm_final = nn.LayerNorm(self.dim_embedding_token)
+
+        # Mamba model
         self.mamba_layers = nn.ModuleList(
             [
                 Mamba(
@@ -48,7 +54,7 @@ class PTRnet(nn.Module):
                     d_conv=config.d_conv,  # Local convolution width
                     expand=config.expand,  # Block expansion factor
                 ).to(self.device)
-                for _ in range(config.num_layers)
+                for _ in range(self.nr_layers)
             ]
         )
 
@@ -64,25 +70,33 @@ class PTRnet(nn.Module):
         else:
             self.predictor = Predictor(config, self.output_dim).to(self.device)
 
-    def forward(self, inputs: Tensor, mask: Tensor = None) -> Tensor:
+    def forward(self, inputs: Tensor) -> Tensor:
         rna_data, tissue_id, seq_lengths = inputs[0], inputs[1], inputs[2]
 
-        tissue_embedding = self.tissue_encoder(tissue_id)  # (batch_size, dim_embedding_token)
-        seq_embedding = self.seq_encoder(rna_data)  # (batch_size, seq_len, dim_embedding_token)
+        tissue_embedding = self.tissue_encoder(tissue_id)  # (batch_size, dim_embedding_tissue)
+        seq_embedding = self.seq_encoder(rna_data)  # (batch_size, seq_len, 4, dim_embedding_token)
 
         # sum up the embeddings of the 4 features (nucleotide, coding_area, sec_struc, loop_type)
-        seq_embedding = seq_embedding.sum(dim=2)
+        if self.seq_only:
+            seq_embedding = seq_embedding[:, :, 0, :]
+        else:
+            seq_embedding = seq_embedding.sum(dim=2)  # (batch_size, seq_len, dim_embedding_token)
 
         # contextualize with tissue embedding
-        x = seq_embedding + tissue_embedding.unsqueeze(1)  # (batch_size, padded_seq_length, dim_embedding_token)
+        x = seq_embedding + tissue_embedding.unsqueeze(1)  # (batch_size, seq_len, dim_embedding_token)
 
-        # Apply padding mask to zero out tissue embedding for padded tokens
+        # Apply padding mask
         padding_mask = (seq_embedding != 0).to(self.device)
         x *= padding_mask
 
-        # Apply Mamba model
-        for layer in self.mamba_layers:
-            x = layer(x)
+        # Pass through Mamba layers with residual connections and layer norm
+        if self.nr_layers > 1:
+            for layer, lnorm in zip(self.mamba_layers, self.mamba_norms):
+                x = lnorm(x)
+                x = layer(x)
+            x = self.layer_norm_final(x)
+        else:
+            x = self.mamba_layers[0](x)
 
         if self.pretrain:
             y_pred = []
@@ -90,14 +104,29 @@ class PTRnet(nn.Module):
                 logits = predictor(x)
                 y_pred.append(F.softmax(logits, dim=-1))
         else:
-            # Extract outputs corresponding to the last valid time step
-            idx = ((seq_lengths - 1).unsqueeze(1).unsqueeze(2).expand(-1, 1, x.size(2)))  # (batch_size, 1, embedding_dim)
-            x_last = x.gather(1, idx).squeeze(1)  # (batch_size, embedding_dim)
+            # Extract last valid time step
+            idx = ((seq_lengths - 1).unsqueeze(1).unsqueeze(2).expand(-1, 1, x.size(2)))
+            x_last = x.gather(1, idx).squeeze(1)
             if self.frequency_features:
                 x_last = torch.cat([x_last, inputs[3]], dim=-1)
             y_pred = self.predictor(x_last)
 
         return y_pred
+
+
+class PTRnetMLP(nn.Module):
+    # TODO
+    pass
+
+
+class PTRnet(nn.Module):
+    def __init__(self, config: DictConfig, device: torch.device):
+        super(PTRnet, self).__init__()
+        self.ptrnet = PTRnetPure(config, device)
+        # self.ptrnet = None
+
+    def forward(self, inputs: Tensor) -> Tensor:
+        return self.ptrnet(inputs)
 
 
 if __name__ == "__main__":
