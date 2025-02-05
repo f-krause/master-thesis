16510b08c4bdbdd431f7844df47a4b26995975bb
Felix Krause <felix_krause1999@yahoo.de>
2025-02-05 23:24:59 +0100

    move output dir specification to separate utils file

---
+++ experiments/visualisation/utils.py
@@ -0,0 +1,16 @@
+import os
+import platform
+
+
+def set_output_dir():
+    if platform.node() == "INSERT_WORK_LAPTOP_ID":
+        OUTPUT_DIR = r"C:\Users\felix.krause\code\uni\master-thesis-text\assets\data_viz"
+    elif platform.node() == "Felix-PC":
+        OUTPUT_DIR = r"C:\Users\Felix\code\uni\UniVie\master-thesis-text\assets\data_viz"
+    else:
+        raise ValueError("Unknown platform")
+
+    os.environ["OUTPUT_DIR"] = OUTPUT_DIR
+
+
+set_output_dir()
