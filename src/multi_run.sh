dd49f97a3f6675d2a1cfd4e73169663022a4f94b
krausef99 <krausef99@univie.ac.at>
2025-05-04 19:15:35 +0200

    updating model run bash files

--- src/multi_run.sh
+++ src/multi_run.sh
@@ -1,4 +1,4 @@
-python main.py --gpu_id 0 --lstm
 python main.py --gpu_id 0 --legnet
-python main.py --gpu_id 0 --baseline  # run vanilla and with freq enabled
+python main.py --gpu_id 0 --lstm
+python main.py --gpu_id 0 --baseline  # run vanilla and with freq enabled!
 python main.py --gpu_id 0 --xlstm  # only works on gpu 0
--- src/multi_run_2.sh
+++ src/multi_run_2.sh
@@ -1,4 +1,5 @@
-python main.py --gpu_id 1 --gru
 python main.py --gpu_id 1 --cnn
-python main.py --gpu_id 1 --transformer
 python main.py --gpu_id 1 --mamba
+python main.py --gpu_id 1 --gru
+python main.py --gpu_id 1 --transformer
+#python main.py --gpu_id 1 --ribonn
