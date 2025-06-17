62e3effecbce49a45475b625cdd4aab70b91e07c
f-krause <felix_krause1999@yahoo.de>
2025-06-17 18:14:01 +0200

    adding work laptop to data viz output paths

--- experiments/visualisation/vis_utils.py
+++ experiments/visualisation/vis_utils.py
@@ -3,7 +3,7 @@ import platform
 
 
 def set_output_dir():
-    if platform.node() == "INSERT_WORK_LAPTOP_ID":
+    if platform.node() == "TGA-NB-060":
         OUTPUT_DIR = r"C:\Users\felix.krause\code\uni\master-thesis-text\assets\data_viz"
     elif platform.node() == "Felix-PC":
         OUTPUT_DIR = r"C:\Users\Felix\code\uni\UniVie\master-thesis-text\assets\data_viz"
